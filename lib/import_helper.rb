fecce2877fa1aaaba781a2cc136d5c7594c4ec0b
jittat <jittat@6386c4cd-e34a-4fa8-8920-d93eb39b512e>
2009-05-25 05:28:04 +0000

    MERGED change set (407:408) from web/judge in branch combined-with-judge (added import_problem_new)
    git-svn-id: http://theory.cpe.ku.ac.th/grader/judge/trunk/scripts@409 6386c4cd-e34a-4fa8-8920-d93eb39b512e

--- lib/import_helper.rb
+++ lib/import_helper.rb
@@ -1,7 +1,7 @@
 
-def filter_filename_for_testrun(testrun, filename_list)
+def filter_filename_for_testrun(testrun, filename_list, raw_prefix='')
   l = []
-  regex = Regexp.new("^(#{testrun}[a-z]*|#{testrun}-.*)$")
+  regex = Regexp.new("^(#{Regexp.escape(raw_prefix)}#{testrun}[a-z]*|#{testrun}-.*)$")
   filename_list.each do |filename|
     if regex.match(filename)
       l << filename
@@ -10,13 +10,13 @@ def filter_filename_for_testrun(testrun, filename_list)
   l
 end
 
-def build_testrun_info(num_testruns, input_filename_list)
+def build_testrun_info(num_testruns, input_filename_list, raw_prefix='')
   info = []
   num_testcases = 0
   num_testruns.times do |i|
     r = i+1
     testrun_info = []
-    filenames = filter_filename_for_testrun(r, input_filename_list)
+    filenames = filter_filename_for_testrun(r,input_filename_list,raw_prefix)
     filenames.each do |fname|
       num_testcases += 1
       testrun_info << [num_testcases,fname]
