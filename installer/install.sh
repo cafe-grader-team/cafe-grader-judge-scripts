eba08f2690046e2b751c2df6f7799fce49536461
Nattee Niparnan <nattee@gmail.com>
2016-08-03 15:31:40 +0700

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -9,12 +9,13 @@ echo "Installing required apts"
 
 sudo apt-get update
 sudo apt-get install mysql-server mysql-client \
-  g++ gcc apache2 libmysqlclient15-dev build-essential \
+  g++ gcc apache2 libmysqlclient20 build-essential \
   git-core openssl libreadline6 libreadline6-dev \
   zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev \
   sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev \
   ncurses-dev automake libtool bison subversion \
-  pkg-config curl nodejs unzip pyflakes ruby default-jdk
+  pkg-config curl nodejs unzip pyflakes ruby default-jdk \
+  libmysqld-dev mercurial python-setuptools python-dev
 
 echo "Installing RVM"
 curl -k -L https://get.rvm.io | bash -s stable
